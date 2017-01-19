875e3fbf42d14f9af5a39e5da8ae514b8fd178e8
cdag <cenobius85@gmail.com>
2017-01-19 15:34:20 -0500

    Add files via upload

---
+++ get.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+url=$1
+
+if [[ -z $url ]]; then
+  echo -e "Imgur  downloader\nUsage: $0 <album url>"
+  exit 1
+fi
+
+if [[ "$url" =~ http://i.imgur.com/ ]]; then
+wget -q "$1" -O -|grep 'post"'|cut -d\" -f2|while read id
+
+do 
+    echo "Downloading $id.jpg"
+    wget -q -c "http://i.imgur.com/$id.jpg"
+done
+fi
---
+++ stemplate.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+printf "\n"
+printf "Script Template Creator\n"
+printf "For bash/lua/python/perl scripts\n"
+printf "Usage: \"b\" bash \"l\" lua \"py\" python \"pe\" perl\n"
+read c1
+case "$c1" in
+	"b"|"B") echo "Enter name for bash script:" &&
+		 read bsfname &&
+		 touch ~/scripts/$bsfname.sh &&
+		 echo -e "#!/bin/bash\n\n" > ~/scripts/$bsfname.sh &&
+		 echo "Bash script template created in ~/scripts" ;;
+
+	"l"|"L") echo "Enter name for lua script:" &&
+		 read lsfname &&
+		 touch ~/scripts/lua/$lsfname.lua &&
+		 echo -e "#!/usr/bin/env lua\n\n" > ~/scripts/lua/$lsfname.lua &&
+		 echo "Lua script template created in ~/scripts/lua" ;;
+
+	"py"|"PY") echo "Enter name for python script:" &&
+		   read pysfname &&
+		   touch ~/scripts/py/$pysfname.py &&
+		   echo -e "#!/usr/bin/env python\n\n" > ~/scripts/py/$pysfname.py &&
+		   echo "Python script template created in ~/scripts/py" ;;
+
+	"pe"|"PE") echo "Enter name for perl script:" &&
+		   read pesfname &&
+		   touch ~/scripts/perl/$pesfname.pl &&
+		   echo -e "#!/usr/bin/env perl\n\n" > ~/scripts/perl/$pesfname.pl &&
+		   echo "Perl script template created in ~/scripts/perl" ;;
+
+	*)	echo "Invalid choice, exiting..." ;;
+
+esac
+exit 0
